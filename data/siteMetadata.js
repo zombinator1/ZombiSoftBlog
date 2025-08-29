4ee6ad12d600d4f2e68b3079c6a3bdc3d383a7b9
Bartek <bzabekk@gmail.com>
2025-08-29 20:55:35 +0200

    Change blog title and author metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  author: 'Bartek Zabek',
+  headerTitle: 'ZombiStackBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
