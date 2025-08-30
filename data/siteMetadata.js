e590949a4ff29be6ff0f90b9a8b88b6af7ed3202
Bartek <bzabekk@gmail.com>
2025-08-30 19:40:54 +0200

    Change favicon, minor siteMetadata adjustments

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logonobg.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
