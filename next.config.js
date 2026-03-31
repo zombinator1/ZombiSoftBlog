d9354d42317a5bd2eeaaae6be723742a2dabe44e
Bartek <bzabekk@gmail.com>
2026-03-31 17:35:53 +0200

    update nextjs

--- next.config.js
+++ next.config.js
@@ -66,6 +66,9 @@ module.exports = () => {
   return plugins.reduce((acc, next) => next(acc), {
     output,
     basePath,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
     reactStrictMode: true,
     trailingSlash: false,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
